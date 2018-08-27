586030f7dda0c566e94da9ed86e5c42f8f2128e5
eMe-404 <838702939>
2018-08-27 17:45:20 +0800

    [Li su & Ya er] chore : init practice-diary

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import PracticeDaily from "./components/PracticeDaily";
+
+
+class App extends Component {
+    render() {
+        return (
+            <PracticeDaily/>
+        );
+    }
+}
+
+export default App;
